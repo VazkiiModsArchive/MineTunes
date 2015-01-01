dc047dc88b0483d532e20bb76befed0bbb3aaab9
Lizbeth Rika <vazkii@hotmail.com>
2015-01-01 17:04:04 +0000

    Libraries.

---
+++ src/main/java/vazkii/minetunes/MineTunes.java
@@ -0,0 +1,9 @@
+package vazkii.minetunes;
+
+import cpw.mods.fml.common.Mod;
+import vazkii.minetunes.lib.LibMisc;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class MineTunes {
+
+}
---
+++ src/main/java/vazkii/minetunes/lib/LibMisc.java
@@ -0,0 +1,10 @@
+package vazkii.minetunes.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "mineTunes";
+	public static final String MOD_NAME = "mineTunes";
+	public static final String BUILD = "GRADLE:BUILD";
+	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
+	
+}
